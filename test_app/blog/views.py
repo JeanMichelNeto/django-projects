334e279d7b72d184f48e5d92418fd73f7eefdc6b
jeanmichelneto <jeanmichelneto@gmail.com>
2022-04-18 17:34:08 -0300

    projects in django

---
+++ test_app/blog/views.py
@@ -0,0 +1,13 @@
+# Create your views here.
+
+from django.views.generic import DetailView, ListView
+
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostDatailView(DetailView):
+    model = Post
